be1cde13fa7720f88ca514c541b14e47e87a2483
elenapleshak-a11y <elena.pleshak@gmail.com>
2025-09-17 22:50:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = 'paytracker-cache-v1';
+const TO_CACHE = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(TO_CACHE)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event=>{
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event=>{
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request).catch(()=>caches.match('/index.html')))
+  );
+});
 
